e535bfe76b2d49cad3ac9fd5a2b702aa5811e4b3
ofosh <foshgraphs@gmail.com>
2025-01-24 15:52:59 +0100

    button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,9 +25,8 @@ export default function Hero() {
                   href={Resume}
                   download="Resume"
                   className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Download Resume
-                </a>
+                ></a>
+                Download Resume
               </button>
 
               <a
